a2e037e369033d146a9af939038545757058a96c
alivcor <abhinandandubey@live.com>
2017-04-26 14:57:38 -0400

    lwc: genJSON

--- parse_json.py
+++ parse_json.py
@@ -1,11 +1,13 @@
 import json
 from pprint import pprint
 import csv
+import unicodedata
 
 cleanData = []
+valDict = {}
 
 def genCleanCSV():
-    global cleanData
+    global cleanData, valDict
     rawData = []
     with open("Datasets/raw/API_EN.ATM.CO2E.PC_DS2_en_csv_v2/API_EN.ATM.CO2E.PC_DS2_en_csv_v2.csv", "rb") as csvfile:
         datareader = csv.reader(csvfile)
@@ -19,6 +21,7 @@ def genCleanCSV():
         newrow = row
         del newrow[2]
         del newrow[2]
+        valDict[newrow[1].encode('ascii','ignore')] = newrow[2:]
         cleanData.append(newrow)
     ofile = open('Datasets/clean/cdo_emissions.csv', "wb")
     writer = csv.writer(ofile)
@@ -26,12 +29,24 @@ def genCleanCSV():
     for row in cleanData:
         writer.writerow(row)
 
+
+def genJSON():
+    global cleanData
+    with open('Datasets/raw/countries.json') as data_file:
+        data = json.load(data_file)
+    print "Loaded", len(data), "countries."
+
+    for country in data:
+        country_code = country["cca3"].encode('ascii','ignore')
+        country_latlng = country["latlng"]
+        try:
+            country_val = valDict[country_code]
+            print "For", country_code, "at", country_latlng, ":"
+            print "\t", country_val
+            print "\n"
+        except KeyError:
+            print "Not found", country_code
+
+
 genCleanCSV()
-
-with open('Datasets/raw/countries.json') as data_file:
-    data = json.load(data_file)
-
-print len(data)
-#
-# for country in data:
-#     if country["cca3"]
+genJSON()
