ccba07fd95ffc3e803a92f889d16be4b0fe7c1b7
alivcor <abhinandandubey@live.com>
2017-04-26 14:43:44 -0400

    changes in cleanCSV function

--- parse_json.py
+++ parse_json.py
@@ -2,32 +2,36 @@ import json
 from pprint import pprint
 import csv
 
+cleanData = []
 
 def genCleanCSV():
     global cleanData
+    rawData = []
     with open("Datasets/raw/API_EN.ATM.CO2E.PC_DS2_en_csv_v2/API_EN.ATM.CO2E.PC_DS2_en_csv_v2.csv", "rb") as csvfile:
         datareader = csv.reader(csvfile)
         for i in range(5):
             next(datareader, None)
         for line in datareader:
-            newline = line
-            del newline[1]
-            del newline[1]
-            del newline[1]
             # print newline
-            if newline[1] != '':
-                cleanData.append(newline[0:55])
-    ofile = open('Datasets/.csv', "wb")
-    writer = csv.writer(ofile, delimiter='', quotechar='"', quoting=csv.QUOTE_ALL)
+            if line[4] != '':
+                rawData.append(line)
+    for row in rawData:
+        newrow = row
+        del newrow[2]
+        del newrow[2]
+        cleanData.append(newrow)
+    ofile = open('Datasets/clean/cdo_emissions.csv', "wb")
+    writer = csv.writer(ofile)
 
-    for row in reader:
+    for row in cleanData:
         writer.writerow(row)
 
+genCleanCSV()
 
-with open('Datasets/countries.json') as data_file:
+with open('Datasets/raw/countries.json') as data_file:
     data = json.load(data_file)
 
 print len(data)
-
-for country in data:
-    if country["cca3"]
+#
+# for country in data:
+#     if country["cca3"]
