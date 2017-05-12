c8d0dab37840735182076f82681d22da687ab280
alivcor <abhinandandubey@live.com>
2017-05-11 21:15:14 -0400

    color shading2

--- map/js/map.js
+++ map/js/map.js
@@ -361,7 +361,8 @@ $(function(){
 //                document.getElementById("bufaso").innerHTML = document.getElementById("bufaso").innerHTML + country_rates[data.iso_a3];
                 color = '#08a35c';
 			    var num = parseInt(color.slice(1),16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, G = (num >> 8 & 0x00FF) + amt, B = (num & 0x0000FF) + amt;
-                return "0x" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+                var finalc =  "0x" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+                document.getElementById("colorsdd").innerHTML = document.getElementById("colorsdd").innerHTML + ' ' + finalc;
 
 
             } else {
